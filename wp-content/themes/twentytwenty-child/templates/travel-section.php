b65fb56dfa36d7d34624345b343ce4c9c403214e
alexandr_k <kalvier34@gmail.com>
2020-03-02 00:14:58 +0200

    typo fix

--- wp-content/themes/twentytwenty-child/templates/travel-section.php
+++ wp-content/themes/twentytwenty-child/templates/travel-section.php
@@ -2,7 +2,7 @@
     <div class="row service-block-wrapper">
         <?php get_template_part( 'templates/travel-section-script-templates' ); ?>
         <div class="col-12 col-sm-4 col-md-3 section-heading" data-bind="text: name"></div>
-        <div class="col-12 col-sm-8 text-right section-links" data-bind="foreach: sortOptions">
+        <div class="col-12 col-sm-8 col-md-9 text-right section-links" data-bind="foreach: sortOptions">
             <a href="#" data-bind="text: $data, css: { active: $data == $parent.sortActive() } , click: $root.updateSortOption.bind($data, $parent.name)"></a>
         </div>
         <?php get_template_part( 'templates/travel-section-content' ); ?>
